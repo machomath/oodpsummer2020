912c59080c7312f865d0360841137cfb2e141306
Dr. Zeb <drzeb@machomath.com>
2020-06-26 16:05:52 -0500

    PHP Mauler

--- phpemail2/emailviaphpmailer.php
+++ phpemail2/emailviaphpmailer.php
@@ -15,20 +15,22 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp1.example.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';//'smtp1.example.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     // SMTP username
-    $mail->Password   = 'secret';                               // SMTP password
+    $mail->Username   = 'aurangzeb.muhammad40@gmail.com';//'user@example.com';                     // SMTP username
+    $mail->Password   = '';//'secret';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    $mail->addAddress('ellen@example.com');               // Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    $mail->setFrom('aurangzeb.muhammad40@gmail.com', 'Mailer');
+    // $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('aristopedia1@gmail.com', 'Joe User');     // Add a recipient
+    // $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+    // $mail->addAddress('ellen@example.com');               // Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
 
     // Attachments
     // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
