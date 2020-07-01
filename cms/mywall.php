34d1f6d60b84d2d059a816e4f4eeee5d3fe60a19
Dr. Zeb <drzeb@machomath.com>
2020-07-01 17:34:07 -0500

    Nave Bar further work

--- cms/mywall.php
+++ cms/mywall.php
@@ -2,6 +2,13 @@
 <html lang="en" dir="ltr">
   <?php require_once  __DIR__ . '/public/layouts/head.php'; ?>
   <?php
+    // if(isset($_SESSION["user_attributes"])){
+    //   echo "<pre>";
+    //   var_dump($_SESSION["user_attributes"]);
+    //   echo "</pre>";
+    // }
+   ?>
+  <?php
     if(!(isset($_SESSION["is_signed_in"]) && $_SESSION["is_signed_in"])){
       $_SESSION["alert_message"] = "Please signin first";
       header("Location: me.php");
--- cms/public/layouts/navbar.php
+++ cms/public/layouts/navbar.php
@@ -4,8 +4,12 @@
   <a href="index.php">Home</a>
   <a href="public.php">Public</a>
   <a href="mywall.php">Mywall</a>
-  <a href="me.php">Me</a>
   ';
+  if($my_session->is_signed_in()){
+    $html .= '<a href="me.php">'. $_SESSION["user_attributes"]["first_name"] .'</a>';
+  }else{
+    $html .= '<a href="me.php">Me</a>';
+  }
   // to let PHP know that the above string is HTML or part of DOM
   $dom = new DOMDocument();
   $dom->loadHTML($html);
